78b169ab1f05de16cc34262ed17e07f320fe9cb8
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-12-14 19:25:56 -0300

    creating user and order routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import orderRouter from './routes/orders.routes';
+import userRouter from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(userRouter);
+app.use(orderRouter);
 
 export default app;
