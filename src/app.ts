856536aca4a7754d3efd35d523d97b7f148ca6a8
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-12-14 19:30:24 -0300

    products route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import orderRouter from './routes/orders.routes';
+import productRouter from './routes/products.routes';
 import userRouter from './routes/users.routes';
 
 const app = express();
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use(userRouter);
 app.use(orderRouter);
+app.use(productRouter);
 
 export default app;
