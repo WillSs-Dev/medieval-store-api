75ada2f9b05ee3837b99750d005703fa3e69ac0c
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-12-15 16:18:22 -0300

    order type created

---
+++ src/services/products.service.ts
@@ -0,0 +1,18 @@
+import db from '../models/connection';
+import ProductsModel from '../models/products.model';
+import Product from '../types/product';
+
+class ProductsService {
+  private model: ProductsModel;
+
+  constructor() {
+    this.model = new ProductsModel(db);
+  }
+
+  public getAll = async (): Promise<Product[]> => {
+    const products = await this.model.getAll();
+    return products;
+  };
+}
+
+export default ProductsService;
